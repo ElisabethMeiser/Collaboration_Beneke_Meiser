1ad7c413daa941e43fbeac1d3607d7cf83b6b6a8
ElisabethMeiser <148713543+ElisabethMeiser@users.noreply.github.com>
2023-10-30 16:56:11 +0100

    Add files via upload
    This is a updated version of LeishiGuideRanking.m which is now naming the identifiers in a different way, calculating the c 4-10 score and is doing a weighted scoring to give the relative C to STOP score a larger handle.

--- LeishiGuideRanking.m
+++ LeishiGuideRanking.m
@@ -39,13 +39,13 @@ GuideID = readtable(fullfile(path_in, filename),'PreserveVariableNames',true);
 %% Step 2: Data Ranking with grouping by GeneID
 
 % Initialize new columns to store results
-GuideID.NumCysteines = zeros(size(GuideID.GuideSeq));
+GuideID.NumCytosine = zeros(size(GuideID.GuideSeq));
 GuideID.NumPossibleStopCodons = zeros(size(GuideID.GuideSeq));
 GuideID.RelativeProbability = zeros(size(GuideID.GuideSeq));
 GuideID.C4to8Score = zeros(size(GuideID.GuideSeq));
 
 
-% Step 1: Calculate NumCysteines in the 'Target' column using a 
+% Step 1: Calculate NumCytosine in the 'Target' column using a 
 % vectorized approach and 
 % Step 2: determine the number of possible stop codons in the 
 % 'GuideCoordinates' column.
@@ -58,7 +58,7 @@ for i = 1:numel(GuideID.GuideTarget)
 
     % Count 'C' occurrences in all textWithinBrackets
     numCsWithinBrackets = sum(cellfun(@(x) sum(x == 'C'), textWithinBrackets));
-    GuideID.NumCysteines(i) = numCsWithinBrackets;
+    GuideID.NumCytosine(i) = numCsWithinBrackets;
 
     % Count '->' occurrences in all textWithinBrackets to calculate NumPossibleStopCodons
     numStopCodons = sum(cellfun(@(x) sum(x == '>'), textWithinBrackets));
@@ -66,9 +66,10 @@ for i = 1:numel(GuideID.GuideTarget)
 end
 
 % Step 3: Calculate the relative probability to generate a stop codon from any C.
-GuideID.RelativeProbability = GuideID.NumPossibleStopCodons ./ GuideID.NumCysteines;
+GuideID.RelativeProbability = GuideID.NumPossibleStopCodons ./ GuideID.NumCytosine;
 
-% Step 4: Check if 'C' is located in positions 4 to 8
+% Step 4: Check if 'C' is located in positions 4 to 8 also override toms
+% column, which was not working
 for i = 1:size(GuideID, 1)
     c_position = strfind(GuideID.GuideSeq{i}, 'C');
     if ~isempty(c_position) && any(c_position >= 4 & c_position <= 8)
@@ -76,14 +77,45 @@ for i = 1:size(GuideID, 1)
     end
 end
 
+%Rename Toms Varible, because the nomenclature is trouble
+if ismember('EditWindow4to8', GuideID.Properties.VariableNames)
+    % Column 'EditWindow4to8' already exists, no need to rename it
+    GuideID.EditWindow4to8 = GuideID.C4to8Score;
+else
+    % Rename 'EditWindow_4-8' to 'EditWindow4to8' if it doesn't exist
+    GuideID = renamevars(GuideID, 'EditWindow_4-8', 'EditWindow4to8');
+    GuideID.EditWindow4to8 = GuideID.C4to8Score;
+end
+
+% Step 4_b: Check if 'C' is located in positions 4 to 10 also override toms
+% column, which was not working
+for i = 1:size(GuideID, 1)
+    c_position = strfind(GuideID.GuideSeq{i}, 'C');
+    if ~isempty(c_position) && any(c_position >= 4 & c_position <= 10)
+        GuideID.C4to10Score(i) = 1;
+    end
+end
+
+%Rename Toms Varible, because the nomenclature is trouble
+if ismember('EditWindow4to10', GuideID.Properties.VariableNames)
+    % Column 'EditWindow4to10' already exists, no need to rename it
+    GuideID.EditWindow4to10 = GuideID.C4to10Score;
+else
+    % Rename 'EditWindow_4-10' to 'EditWindow4to10' if it doesn't exist
+    GuideID = renamevars(GuideID, 'EditWindow_4-10', 'EditWindow4to10');
+    GuideID.EditWindow4to10 = GuideID.C4to10Score;
+end
+
 %% Calculate the final scores for all sequences in the table
-scores = (GuideID.RelativeProbability + GuideID.C4to8Score + GuideID.("TargetWithinFirst_20%_of_CDS")+ GuideID.("TargetWithinFirst_40%_of_CDS") + GuideID.("EditWindow_4-10"));
+scores = ((GuideID.RelativeProbability * 4) + GuideID.C4to8Score + GuideID.("TargetWithinFirst_20%_of_CDS")+ GuideID.("TargetWithinFirst_40%_of_CDS"));
 
 %normalize scores by maximal possible score = 100
-normalized_scores = scores/5; %num of contributions
+normalized_scores = scores*100/7; %num of contributions
 
 % Add the normalized_scores as a new column 'C_Scores' to the table
-GuideID.C_Scores = normalized_scores;
+GuideID.C_Scores = 100-normalized_scores; % 0 is the best guide, 100 is 
+% the worst guide, counter intuitive, but this way Tom can
+% sort the identidiers in a acending way...
 
 
 %% Create a Guide ID called 'NewIdentifier' that contains all important
@@ -93,6 +125,8 @@ GuideID.C_Scores = normalized_scores;
 % Initialize a cell array to store the 'NewIdentifier' values
 NewIdentifier = cell(size(GuideID, 1), 1);
 
+GuideID.C_Scores_round = round(GuideID.C_Scores); % Only for naming
+
 for i = 1:size(GuideID, 1)
     % Extract 'GeneID' value for the current row
     geneID = GuideID.GeneID{i};
@@ -101,7 +135,11 @@ for i = 1:size(GuideID, 1)
     strand = GuideID.Strand{i};
 
     % Extract 'C_Scores' value for the current row
-    cScore = GuideID.C_Scores(i);
+    cScore = GuideID.C_Scores_round(i);
+
+    % Extract guide coordinates
+    guideCoordinatesValue = GuideID.GuideCoordinates{i};
+    
 
     % Determine the strand part of the 'NewIdentifier'
     strandPart = '';
@@ -112,7 +150,7 @@ for i = 1:size(GuideID, 1)
     end
 
     % Create the 'NewIdentifier' by concatenating the parts
-    NewIdentifier{i} = [geneID '_' strandPart '_' num2str(cScore)];
+    NewIdentifier{i} = [geneID '_'  num2str(cScore) '_' guideCoordinatesValue '_' strandPart];
 end
 
 % Add the 'NewIdentifier' to the 'GuideID' table
