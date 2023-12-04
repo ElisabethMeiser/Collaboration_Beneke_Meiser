9edea8763a98c2d000ac34a374792a49774f0c91
ElisabethMeiser <148713543+ElisabethMeiser@users.noreply.github.com>
2023-12-04 15:30:40 +0100

    Update Call_sorting.m
    update, in progress

--- Call_sorting.m
+++ Call_sorting.m
@@ -1,26 +1,22 @@
-%LeishiGuideRanking(path_in, path_out, filename, rankByGeneID)
-
-%LeishiGuideRanking('/Users/eli/Desktop/TOM_Leish_Guide/', '/Users/eli/Desktop/TOM_Leish_Guide/TestSorting', 'TriTrypDB-59_LmexicanaMHOMGT2001U1103_Genome_LeishBASEedit_v1_output.primer.txt', false)
-
 % Specify the folder path
-folderPath = '/Users/eli/Desktop/TOM_Leish_Guide/GuideIDTable';
+folderPath = '/Volumes/E_Meiser/LeishBASE';
 
 % Other parameters for LeishiGuideRanking
-pathIn = '/Users/eli/Desktop/TOM_Leish_Guide/';
-pathOut = '/Users/eli/Desktop/TOM_Leish_Guide/Sorting120423';
+path_in = '/Volumes/E_Meiser/LeishBASE';
+path_out = '/Volumes/E_Meiser/LeishBASE/Sorting120423';
 rankByGeneID = false ;
 
 % List all files in the folder
-files = dir(fullfile(folderPath, '*.txt')); % Replace 'your_file_extension' with the actual file extension
+files = dir(fullfile(folderPath, '*LeishBASEedit_v1_output.primer.txt')); % Replace 'your_file_extension' with the actual file extension
 
 % Iterate over each file
 for i = 1:length(files)
     % Get the current filename
-    currentFile = files(i).name;
+    filename = files(i).name;
     
     % Construct the full path to the file
-    fullPath = fullfile(folderPath, currentFile);
+    fullPath = fullfile(folderPath, filename);
     
     % Call your LeishiGuideRanking function for each file
-    LeishiGuideRanking(pathIn, pathOut, fullPath, rankByGeneID);
+    LeishiGuideRanking(path_in, path_out, fullPath, rankByGeneID);
 end
